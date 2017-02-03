da02d932faa35d4762e0811bcb80cd1f56c8643a
Xavier Chopin <xchopin54@gmail.com>
2017-02-03 14:00:49 +0100

    Show the place of the user's IP address

--- index.php
+++ index.php
@@ -37,22 +37,20 @@ $xslt->importStylesheet( $XSL );
 print $xslt->transformToXML( $XML );
 
 
-echo <<< EOB
+echo "
 
 <script>
 
 
-	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	var mymap = L.map('mapid').setView([$location->Latitude, $location->Longitude], 13);
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoieGNob3BpbiIsImEiOiJjaXluMXg0cTAwMDBwM3VwZnN0Y2ZxM3JmIn0.Hfr7AY-5aNpongUyCXdOUg', {
 		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		attribution: 'Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, ' +
+			'<a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, ' +
+			'Imagery © <a href=\"http://mapbox.com\">Mapbox</a>',
 		id: 'mapbox.streets'
-	}).addTo(mymap);
+	}).addTo(mymap)
 
 
-</script>
-
-EOB;
+</script>";
